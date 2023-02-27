14104a15567c6967525af25c5d1545330adc1edb
Clément Michelet <clement.michelet@hephaist.io>
2023-02-27 16:21:42 +0100

    refactor: Switch to HTTP handler instead of anonymous function
    See https://bref.sh/docs/function/handlers.html#api-gateway-http-events

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-return function ($event) {
-    return 'Hello ' . ($event['name'] ?? 'world');
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+return new class implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $name = $request->getQueryParams()['name'] ?? 'world';
+
+        return new Response(200, [], sprintf('Hello %s', $name));
+    }
 };
